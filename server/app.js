f279110716d27faede9c9b20c58567720dddc75b
Tom Lynch <me@unknowndomain.co.uk>
2016-05-24 12:23:41 +0100

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,50 @@
+var express = require( 'express' ),
+	app = express(),
+	http = require( 'http' ).Server( app );
+
+var io = require( 'socket.io' )( http );
+
+var serialPort = require( 'serialport' );
+var SerialPort = serialPort.SerialPort;
+var serial;
+
+app.get( '/', function( req, res ) {
+	res.sendFile( __dirname + '/views/index.html' );
+} );
+
+app.use( '/static', express.static( 'static' ) );
+app.use( '/videos', express.static( 'videos' ) );
+
+io.on( 'connection', function( socket ) {
+	console.log( 'Browser connected' );
+
+	socket.on( 'disconnect', function() {
+		console.log( 'Browser disconnected' );
+	} );
+
+	socket.on( 'playing', function( msg ) {
+		console.log( 'Playing: ' + msg );
+	} );
+
+	socket.on( 'paused', function( msg ) {
+		console.log( 'Paused: ' + msg );
+	} );
+
+	socket.on( 'ended', function( msg ) {
+		console.log( 'Ended' );
+	} );
+} );
+
+http.listen( 3000 );
+
+serialPort.list( function ( err, ports ) {
+	ports.forEach( function( port ) {
+		if ( ! serial && port.comName.indexOf( "cu.usb" ) != -1 ) {
+			console.log( '\nConnecting to: "' + port.comName + '"...' );
+			serial = new SerialPort( port.comName, { baudrate: 9600 } );
+			serial.on( 'open', function () {
+
+			} );
+		}
+	} );
+} );
---
+++ server/static/script.js
@@ -0,0 +1,33 @@
+var video, current_video;
+var socket = io();
+
+window.onload = function() {
+	video = document.getElementsByTagName('video')[0];
+
+	video.addEventListener( 'play', function() {
+		socket.emit( 'playing', current_video );
+	} );
+
+	video.addEventListener( 'pause', function() {
+		socket.emit( 'paused', current_video );
+	} );
+
+	video.addEventListener( 'ended', function() {
+		socket.emit( 'ended' );
+		current_video = null;
+	} );
+
+	socket.on( 'play', function( msg ) {
+		if ( msg != current_video ) {
+			current_video = msg;
+			video.src = '/videos/' + msg + '.mov';
+			video.play();
+		} else {
+			video.play();
+		}
+	} );
+	socket.on( 'pause', function( msg ) {
+		if ( current_video != null )
+			video.pause();
+	} );
+}
