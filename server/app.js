dd770adbae448a20268e5b1e18021e677e13e138
Tom Lynch <me@unknowndomain.co.uk>
2016-05-24 12:48:56 +0100

    Support arduino input

--- server/app.js
+++ server/app.js
@@ -41,9 +41,16 @@ serialPort.list( function ( err, ports ) {
 	ports.forEach( function( port ) {
 		if ( ! serial && port.comName.indexOf( "cu.usb" ) != -1 ) {
 			console.log( '\nConnecting to: "' + port.comName + '"...' );
-			serial = new SerialPort( port.comName, { baudrate: 9600 } );
+			serial = new SerialPort( port.comName, {
+				baudrate: 115200,
+				praser: serialPort.parsers.readline( '\n' )
+			} );
 			serial.on( 'open', function () {
-
+				serial.on( 'data', function( data ) {
+					var data = data.toString().trim();
+					if ( data.length == 2 )
+						io.emit( 'play', data );
+				} );
 			} );
 		}
 	} );
